eed29278acbd171fd639b6c6a3aa74e34ac8fc85
Ozodbek00 <kamoliddinovozodbek@mail.ru>
2022-12-19 17:57:44 +0500

    UserService

--- TestTaskMVC/Controllers/UsersController.cs
+++ TestTaskMVC/Controllers/UsersController.cs
@@ -1,12 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TestTaskMVC.Data.Services;
+using TestTaskMVC.Models;
 
 namespace TestTaskMVC.Controllers
 {
-    public class UsersController1 : Controller
+    public class UsersController : Controller
     {
+        private readonly IGenericService<User> service;
+
+        public UsersController(IGenericService<User> service)
+        {
+            this.service = service;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(User user)
+        {
+            return View(await service.CreateAsync(user));
+        }
+
+        [HttpPut, Authorize(Roles = "Admin")]
+        [Authorize(Roles = "User")]
+        public async Task<IActionResult> UpdateAsync(long id, User user)
+        {
+            return View(await service.UpdateAsync(id, user));
+        }
+
+        [HttpDelete, Authorize(Roles = "Admin")]
+        [Authorize(Roles = "User")]
+        public async Task<IActionResult> DeleteAsync(long id)
+        {
+            await service.DeleteAsync(id);
+
+            return View("Done");
+        }
+
+
+        [HttpGet, Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var users = await service.GetAllAsync();
+            if (users.Length == 1)
+                return View("Users not found!");
+
+            return View(users);
+        }
+
+        public async Task<IActionResult> GetByIdAsync(long id)
+        {
+            return View(await service.GetById(id));
         }
     }
 }
